0d3a81831aa496830c55c8658ef963dbc407f132
Ben Gonzales <benedicto.gonzales@hp.com>
2021-05-27 09:12:40 -0500

    init

---
+++ src/main/java/com/magnustiberius/andrea/yacc/YaccApplication.java
@@ -0,0 +1,13 @@
+package com.magnustiberius.andrea.yacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magnustiberius/andrea/yacc/YaccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnustiberius.andrea.yacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
