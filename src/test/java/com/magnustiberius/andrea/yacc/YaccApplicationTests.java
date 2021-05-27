c3aa375ba1a185308946ec34e287a62ca317375e
Ben Gonzales <benedicto.gonzales@hp.com>
2021-05-27 10:02:36 -0500

    init

--- src/test/java/com/magnustiberius/andrea/yacc/YaccApplicationTests.java
+++ src/test/java/com/magnustiberius/andrea/yacc/YaccApplicationTests.java
@@ -1,13 +1,40 @@
 package com.magnustiberius.andrea.yacc;
 
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.magnustiberius.andrea.yacc.lexer.Tokenizer;
 
 @SpringBootTest
 class YaccApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Autowired
+	Tokenizer tokenizer;
+	
+//	@Test
+//	void contextLoads() {
+//	}
 
+	
+	
+	@Test
+	public void givenFileNameAsAbsolutePath_whenUsingClasspath_thenFileData() throws IOException {
+		tokenizer = new Tokenizer();
+	    String expectedData = "Hello, world!";
+	    
+	    Class clazz = YaccApplicationTests.class;
+	    InputStream inputStream = clazz.getResourceAsStream("/test.andreayacc");
+	    String data = tokenizer.readFromInputStream(inputStream);
+
+	    //Assert.assertThat(data, containsString(expectedData));
+	}	
+	
 }
