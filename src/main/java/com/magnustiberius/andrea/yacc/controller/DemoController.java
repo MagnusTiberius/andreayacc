7ba7a796f84fdf5de89f5b0abcfd40fcb35659d2
Ben Gonzales <benedicto.gonzales@hp.com>
2021-05-31 13:31:53 -0500

    add js, css

--- src/main/java/com/magnustiberius/andrea/yacc/controller/DemoController.java
+++ src/main/java/com/magnustiberius/andrea/yacc/controller/DemoController.java
@@ -1,12 +1,27 @@
 package com.magnustiberius.andrea.yacc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.magnustiberius.andrea.yacc.dao.User;
 
 @Controller
 public class DemoController {
-   @RequestMapping(value = "/index")
+   
+	@RequestMapping(value = "/index")
    public String index() {
       return "index";
    }
+   
+   @RequestMapping(value="/save", method=RequestMethod.POST)    
+   public ModelAndView save(@ModelAttribute User user)  
+   {    
+   ModelAndView modelAndView = new ModelAndView();    
+   modelAndView.setViewName("user-data");        
+   modelAndView.addObject("user", user);      
+   return modelAndView;    
+   }    
 }
